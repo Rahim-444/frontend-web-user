0995f46373987ddb626c248bfda4bea5c64c7ca7
Rahim-444 <abderrahimbelkacemi33@gmail.com>
2024-06-26 01:12:28 +0100

    minor changes

--- app/(root)/settings/referral-program/page.tsx
+++ app/(root)/settings/referral-program/page.tsx
@@ -1,4 +1,4 @@
-import SettingsTitle from '../_Componenets/SettingsTitle';
+import SettingsTitle from '@/components/ui/SettingsTitle';
 
 export default function Page() {
   return <SettingsTitle title="Referral Program" />;
